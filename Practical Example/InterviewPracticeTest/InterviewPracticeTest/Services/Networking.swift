e03393b5076e200cf7d9816b864ed90d5d755fd8
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-17 22:01:30 -0800

    Creates enum for NetworkError to define multiple error types.

--- Practical Example/InterviewPracticeTest/InterviewPracticeTest/Services/Networking.swift
+++ Practical Example/InterviewPracticeTest/InterviewPracticeTest/Services/Networking.swift
@@ -7,6 +7,33 @@
 
 import Foundation
 
+enum NetworkError: Swift.Error, CustomStringConvertible {
+  case network
+  case parsing
+  case unknown
+  
+  var description: String {
+    switch self {
+    case .network: return "A network error occured."
+    case .parsing: return "Unable to parse server response."
+    case .unknown: return "An unkown error occured."
+    }
+  }
+  
+  init(_ error: Swift.Error) {
+    switch error {
+    case is NetworkError: self = .network
+    case is DecodingError: self = .parsing
+    default: self = .unknown
+    }
+  }
+}
+
 final class Networking {
   
+  public func fetchData(from urlString: String) {
+//    guard let url = URL(string: urlString) else { }
+//    URLSession.shared.dataTaskPublisher(for: url)
+//      .tryMap(<#T##transform: ((data: Data, response: URLResponse)) throws -> T##((data: Data, response: URLResponse)) throws -> T#>)
+  }
 }
