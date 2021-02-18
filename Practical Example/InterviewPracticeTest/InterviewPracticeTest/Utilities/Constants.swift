08c7f7e1bafec7b34525cf098c5a770ce1b6e911
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-18 13:29:33 -0800

    Creates a constants file to add project level constants.

---
+++ Practical Example/InterviewPracticeTest/InterviewPracticeTest/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  InterviewPracticeTest
+//
+//  Created by Shruti Sharma on 2/17/21.
+//
+
+import Foundation
+
+enum APIEndPoints {
+  static let articlesAPI =  "https://github.com/raywenderlich/ios-interview/blob/master/Practical%20Example/articles.json"
+  static let videosAPI = "https://github.com/raywenderlich/ios-interview/blob/master/Practical%20Example/videos.json"
+}
+
