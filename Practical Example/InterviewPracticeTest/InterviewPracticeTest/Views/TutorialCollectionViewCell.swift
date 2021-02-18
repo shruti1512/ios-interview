fabde075df5b81a7d503e8194359d3a02f824c5c
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-17 17:11:07 -0800

    Adds class for TutorialCollectionViewCell and creates subviews for title, description, image, etc.

--- Practical Example/InterviewPracticeTest/InterviewPracticeTest/Views/TutorialCollectionViewCell.swift
+++ Practical Example/InterviewPracticeTest/InterviewPracticeTest/Views/TutorialCollectionViewCell.swift
@@ -8,36 +8,95 @@
 import UIKit
 
 class TutorialCollectionViewCell: UICollectionViewCell {
-  
-  
-  lazy var nameLabel: UILabel = {
+    
+  private lazy var nameLabel: UILabel = {
     let label = UILabel(frame: .zero)
-    label.font = .preferredFont(forTextStyle: .title1)
+    label.font = .preferredFont(forTextStyle: .headline)
     label.textColor = .label
     label.numberOfLines = 2
+//    label.backgroundColor = .systemGreen
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
   
-  lazy var descriptionLabel: UILabel = {
+  private lazy var techLabel: UILabel = {
     let label = UILabel(frame: .zero)
-    label.font = .preferredFont(forTextStyle: .title1)
-    label.textColor = .label
-    label.numberOfLines = 2
+    label.font = .preferredFont(forTextStyle: .body)
+    label.textColor = .secondaryLabel
+    label.numberOfLines = 1
+//    label.backgroundColor = .systemYellow
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
+
+  private lazy var contentTypeLabel: UILabel = {
+    let label = UILabel(frame: .zero)
+    label.font = .preferredFont(forTextStyle: .body)
+    label.textColor = .secondaryLabel
+    label.numberOfLines = 1
+    label.translatesAutoresizingMaskIntoConstraints = false
+//    label.backgroundColor = .systemTeal
+    return label
+  }()
+
+  private lazy var descriptionLabel: UILabel = {
+    let label = UILabel(frame: .zero)
+    label.font = .preferredFont(forTextStyle: .body)
+    label.textColor = .secondaryLabel
+    label.numberOfLines = 2
+    label.translatesAutoresizingMaskIntoConstraints = false
+//    label.backgroundColor = .systemGray
+    return label
+  }()
   
-  lazy var artworkImgView: UIImageView = {
+  private lazy var artworkImgView: UIImageView = {
     let imageView = UIImageView(frame: .zero)
     imageView.image = UIImage(systemName: "photo")
     imageView.contentMode = .scaleAspectFit
     return imageView
   }()
   
+  private lazy var titleStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [nameLabel, techLabel])
+    stackView.axis = .vertical
+    return stackView
+  }()
+  
+  private lazy var topStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [titleStackView, artworkImgView])
+    stackView.axis = .horizontal
+    return stackView
+  }()
+
+  private lazy var bottomStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [descriptionLabel, contentTypeLabel])
+    stackView.axis = .vertical
+    return stackView
+  }()
+
+  private lazy var parentStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [topStackView, descriptionLabel, contentTypeLabel])
+    stackView.setCustomSpacing(10, after: techLabel)
+    stackView.setCustomSpacing(10, after: descriptionLabel)
+    stackView.axis = .vertical
+    return stackView
+  }()
+
+  private lazy var cardView: CardView = {
+    let card = CardView(frame: .zero)
+    return card
+  }()
+  
+//  private lazy var testView: UIView = {
+//    let view = UIView(frame: .zero)
+//    view.backgroundColor = .systemBlue
+//    return view
+//  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupViews()
+    setupLayout()
   }
   
   required init?(coder: NSCoder) {
@@ -45,7 +104,9 @@ class TutorialCollectionViewCell: UICollectionViewCell {
   }
   
   private func setupViews() {
-    addSubviews([nameLabel, descriptionLabel, artworkImgView])
+    addSubview(cardView)
+    cardView.addSubview(parentStackView)
+//    cardView.addSubview(testView)
   }
   
   private func setupLayout() {
