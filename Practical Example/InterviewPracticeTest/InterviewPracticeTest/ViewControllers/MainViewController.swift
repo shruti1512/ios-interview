addc2ebd7ebc1afe78a2eafde1455e8f650f75ff
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-17 17:13:13 -0800

    Configure collection view data source and apply snapshot.

--- Practical Example/InterviewPracticeTest/InterviewPracticeTest/ViewControllers/MainViewController.swift
+++ Practical Example/InterviewPracticeTest/InterviewPracticeTest/ViewControllers/MainViewController.swift
@@ -31,9 +31,49 @@ class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setupCollectionView()
+    configureCollectionViewDataSource()
+    reloadData()
+  }
+  
+  private func setupCollectionView() {
+    view.addSubview(collectionView)
+  }
+  
+  private func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                          heightDimension: .fractionalHeight(1))
+    let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+    
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                           heightDimension: .estimated(205))
+    let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                       subitems: [layoutItem])
+    
+    let section = NSCollectionLayoutSection(group: layoutGroup)
+    return UICollectionViewCompositionalLayout(section: section)
   }
 
-
+  private func configureCollectionViewDataSource() {
+    dataSource = CollectionViewDataSource(collectionView: collectionView) { (collectionView, indexPath, tutorial) -> UICollectionViewCell? in
+      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
+                                                            TutorialCollectionViewCell.reuseIdentifier,
+                                                          for: indexPath) as? TutorialCollectionViewCell else { fatalError("Unable to dequeu cell")  }
+      cell.configure(with: tutorial)
+      return cell
+    }
+  }
+  
+  private func reloadData() {
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Tutorial>()
+    snapshot.appendSections([.main])
+    for i in 1...10 {
+      let tutorial = Tutorial(name: "\(i): Mac Catalyst with Andy Pereira – Podcast S10 E7",
+                             artwork: "", description: "Dru flips the switch on his 50th show with Andy Pereira telling us how easy Catalyst can be. After Dru talks about transitioning from developer to architect.", type: .article)
+      snapshot.appendItems([tutorial])
+    }
+    dataSource?.apply(snapshot)
+  }
+  
 }
 
