ed4f843a2858885f42d73c163f2cd37935388683
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-17 17:12:27 -0800

    Set up and configure collection view in MainViewController.

--- Practical Example/InterviewPracticeTest/InterviewPracticeTest/ViewControllers/MainViewController.swift
+++ Practical Example/InterviewPracticeTest/InterviewPracticeTest/ViewControllers/MainViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+  enum Section {
+    case main
+  }
+  
+//  private var tutorials = [Tutorial(name: <#T##String#>, artwork: <#T##String#>, description: <#T##String#>, type: <#T##ContentType#>)]
+  
+  typealias CollectionViewDataSource = UICollectionViewDiffableDataSource<Section, Tutorial>
+  var dataSource: CollectionViewDataSource?
+  
+  private lazy var collectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: view.bounds,
+                                          collectionViewLayout: configureCollectionViewLayout())
+    collectionView.backgroundColor = .systemBackground
+    collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    collectionView.register(TutorialCollectionViewCell.self,
+                            forCellWithReuseIdentifier: TutorialCollectionViewCell.reuseIdentifier)
+    return collectionView
+  }()
+  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
